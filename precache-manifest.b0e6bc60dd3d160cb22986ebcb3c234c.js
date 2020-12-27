55192f1c22313f2def12608d8f943e2c5abaaef5
Priyanshu Vishnoi <priyanshuvishnoi990@gmail.com>
2020-12-27 16:16:48 +0530

    Updates

---
+++ precache-manifest.b0e6bc60dd3d160cb22986ebcb3c234c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40bf40c3ae42b1d9a62ce8c11b32410",
+    "url": "/reactapp_confusion/index.html"
+  },
+  {
+    "revision": "dd1f4fe4ff2e052433bd",
+    "url": "/reactapp_confusion/static/css/2.89d1178e.chunk.css"
+  },
+  {
+    "revision": "d652b5499d9617ac2184",
+    "url": "/reactapp_confusion/static/css/main.a8378c22.chunk.css"
+  },
+  {
+    "revision": "dd1f4fe4ff2e052433bd",
+    "url": "/reactapp_confusion/static/js/2.4dba1f5f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactapp_confusion/static/js/2.4dba1f5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d652b5499d9617ac2184",
+    "url": "/reactapp_confusion/static/js/main.30fa38d4.chunk.js"
+  },
+  {
+    "revision": "3e853938112e86124707",
+    "url": "/reactapp_confusion/static/js/runtime-main.717a9c88.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactapp_confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactapp_confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactapp_confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactapp_confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactapp_confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
